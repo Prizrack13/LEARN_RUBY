fd5aed882e2088323d46865c05a23ae7ed5742cf
Anatoliy <Prizrack13@mail.ru>
2013-02-04 14:13:16 +0200

    %10%
    1 class

---
+++ 10_timer/timer.rb
@@ -0,0 +1,15 @@
+class Timer
+	attr_accessor :seconds  
+	
+	def initialize
+		@seconds = 0
+	end	
+	
+	def time_string
+		padded(seconds/3600)+":"+padded(seconds/60%60)+":"+padded(seconds%60)
+	end	
+	
+	def padded(s)
+		s<10 ? "0"+s.to_s : s.to_s
+	end	
+end
